ce11dcf03b1ab918a1a63174a1bedbb945808125
Robert Haken <haken@havit.cz>
2024-05-20 00:13:51 +0200

    DocumentStructure

---
+++ DocumentStructure/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.RegularExpressions;
+
+string input = """
+	{START:outer}
+		text
+		{START:inner}text{END:inner}
+	{END:outer}
+	""";
+
+Console.WriteLine(IsValidStructure(input) ? "VALID" : "INVALID");
+
+bool IsValidStructure(string text)
+{
+	var stack = new Stack<string>();
+	var regex = new Regex(@"\{(START|END):([^\}]+)\}");
+	var matches = regex.Matches(text);
+
+	foreach (Match match in matches)
+	{
+		string tag = match.Groups[1].Value;
+		string name = match.Groups[2].Value;
+
+		if (tag == "START")
+		{
+			stack.Push(name);
+		}
+		else if (tag == "END")
+		{
+			if (stack.Count == 0 || stack.Pop() != name)
+			{
+				return false;
+			}
+		}
+	}
+
+	return stack.Count == 0;
+}
+
